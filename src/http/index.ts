997f39e69de4746d0d5e927af4d46880c2a8cece
crotaliu <crotaliu@tencent.com>
2022-08-14 16:22:41 +0800

    chore: http配置

--- src/http/index.ts
+++ src/http/index.ts
@@ -4,18 +4,17 @@ import { ElMessage } from 'element-plus';
 import { AxiosSourceCancelConfig, AxiosResponseConfig } from '@/types';
 
 class HttpClient {
-  commonOption; // AxiosRequestConfig
-
-  axios; // AxiosInstance
-
+  // Axios 请求通用配置
+  commonOption;
+  // Axios 实例
+  axios;
+  // 取消请求
   sourceCancel: AxiosSourceCancelConfig = { token: undefined, cancel: null };
-
-  redirecting = false;
 
   constructor(commonOption: Object, options: Object) {
     this.commonOption = commonOption;
     this.axios = axios.create({
-      timeout: 150000,
+      timeout: 60000,
       cancelToken: this.sourceCancel.token,
       ...options,
     });
@@ -23,21 +22,15 @@ class HttpClient {
     this.axios.interceptors.request.use(this.requestSuccessHandle, this.requestErrorHandle);
     this.axios.interceptors.response.use(this.responseSuccessHandle, this.responseErrorHandle);
   }
-
+  // 请求正常回调
   requestSuccessHandle = (config: AxiosRequestConfig) => ({
     ...config,
-    headers: {
-      'x-requested-with': 'XMLHttpRequest',
-    },
+    headers: { 'x-requested-with': 'XMLHttpRequest' },
   });
-
-  requestErrorHandle = (e: AxiosError) => {
-    console.error(e);
-    return Promise.reject(e);
-  };
-
+  // 请求失败回调
+  requestErrorHandle = (e: AxiosError) => Promise.reject(e);
+  // 回包成功回调
   responseSuccessHandle = (response: AxiosResponseConfig<any>) => {
-    // 后台返回数据中有token,就应该将token放在header中；
     let msg = '未知错误';
     if (!response || !response.data) {
       msg = '网络错误';
@@ -45,96 +38,23 @@ class HttpClient {
       msg = response.msg;
     }
 
-    ElMessage({
-      type: 'error',
-      message: response.data.msg || msg,
-    });
-
-    console.error(
-      '------------------------------\n',
-      '接口错误:',
-      msg,
-      '\n',
-      '接口名:',
-      response.config.baseURL + response.config.url,
-      '\n',
-      '参数:',
-      response.config.data,
-      '\n',
-      '返回值:',
-      response.data,
-      '\n',
-      '------------------------------\n',
-    );
-
+    ElMessage({ type: 'error', message: response.data.msg || msg });
     return Promise.reject(response.data);
   };
-
+  // 回包失败回调
   responseErrorHandle = (e: AxiosError) => {
     let msg = '未知错误';
-    if (axios.isCancel(e)) {
-      return Promise.reject(e);
-    }
+    if (axios.isCancel(e)) return Promise.reject(e);
     if (!e?.response || !e.response.data) {
       msg = '网络错误';
     }
 
-    ElMessage({
-      type: 'error',
-      message: msg,
-    });
-
-    console.error(
-      '------------------------------\n',
-      '接口错误:',
-      e.message,
-      '\n',
-      '接口名:',
-      (e.config.baseURL as string) + e.config?.url,
-      '\n',
-      '参数:',
-      e.config.data,
-      '\n',
-      '------------------------------\n',
-    );
+    ElMessage({ type: 'error', message: msg });
     return Promise.reject(e);
   };
 
   /**
-   * @param config AxiosRequestConfig
-   */
-  setCommonOption = (config: object) => {
-    this.commonOption = Object.assign(this.commonOption, config);
-  };
-
-  changeSource = (e: AxiosSourceCancelConfig) => {
-    this.sourceCancel = e;
-    this.setRequestConfig({
-      cancelToken: this.sourceCancel.token,
-    });
-  };
-
-  getCommonOption() {
-    return { ...this.commonOption };
-  }
-
-  /**
-   * @param config AxiosRequestConfig
-   */
-  setRequestConfig = (config: AxiosRequestConfig) => {
-    this.axios.defaults = Object.assign(this.axios.defaults, config);
-  };
-
-  /**
-   * @param onFulfilled (config:AxiosRequestConfig) => AxiosRequestConfig
-   * @param onRejected (error : any) => any
-   */
-  setRequestInterceptor = (onFulfilled: any, onRejected: any) => {
-    this.axios.interceptors.request.use(onFulfilled, onRejected);
-  };
-
-  /**
-   *
+   * @description get post put delete 请求方法
    * @param {*} url string
    * @param {*} option AxiosRequestConfig
    */
@@ -148,27 +68,6 @@ class HttpClient {
 
   delete = (url: string, option: AxiosRequestConfig = {}) =>
     this.axios.delete(url, { ...this.commonOption, ...option });
-
-  download = (url: string, option: AxiosRequestConfig) => {
-    let resUrl = url;
-    let params = '';
-    if (option?.params) {
-      Object.keys(option.params).forEach((key) => {
-        if (option.params[key]) {
-          params += `${key}=${option.params[key]}&`;
-        }
-      });
-    }
-    if (params) {
-      resUrl = `${url}?${params.slice(0, params.length - 1)}`;
-    }
-    const a = document.createElement('a');
-    a.style.display = 'none';
-    a.href = resUrl;
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  };
 }
 
 export default new HttpClient(
